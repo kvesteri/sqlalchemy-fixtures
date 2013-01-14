91ffba462b015833b8f2fc1a37a3a063aadc0bf2
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-01-14 15:18:24 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class PyTest(Command):
 
 setup(
     name='sqlalchemy-fixtures',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/kvesteri/sqlalchemy-fixtures',
     license='BSD',
     author='Konsta Vesterinen',
